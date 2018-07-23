0b5f7bd912a659858fa39d12992f3d090a51b16b
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-07-23 16:01:05 -0500

    Create relationships

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,21 +1,28 @@
 const User = require('./user')
+const Product = require('./product')
 const Review = require('./review')
+const Category = require('./category')
+const Manufacturer = require('./manufacturer')
+const Seller = require('./seller')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+// user
+User.hasMany(Review)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+// product
+Product.hasMany(Category)
+Product.hasMany(Review)
+Product.hasOne(Manufacturer)
+Product.hasOne(Seller)
+
+// review
+Review.hasOne(User)
+Review.hasOne(Product)
 
 module.exports = {
   User,
-  Review
+  Product,
+  Review,
+  Category,
+  Manufacturer,
+  Seller
 }
--- server/db/models/seller.js
+++ server/db/models/seller.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Seller = db.define('manufacturer', {
-    name: {
-        type: Sequelize.STRING
-    }
+const Seller = db.define('seller', {
+  name: {
+    type: Sequelize.STRING
+  }
 })
 
 module.exports = Seller
