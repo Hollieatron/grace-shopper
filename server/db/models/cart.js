9f144533d503aa8b2a42bc5826b011990c6a43a2
Eliot Davis <edavis221@gmail.com>
2018-07-26 13:56:26 -0500

    models completed, testing still in progress

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,18 +2,14 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Cart = db.define('cart', {
-  userid: {
-    type: Sequelize.INTEGER
-  },
   guest: {
     type: Sequelize.BOOLEAN,
     defaultValue: false
   },
-  product_ids: {
+  productIds: {
       type: Sequelize.ARRAY(Sequelize.INTEGER),
       defaultValue: []
   }
-
 })
 
 module.exports = Cart
--- server/db/models/cart.spec.js
+++ server/db/models/cart.spec.js
@@ -3,22 +3,35 @@
 'use strict'
 
 const expect = require('chai').expect
-const {Cart} = require('./index')
+const {Cart, User} = require('./index')
 const db = require('../index')
 describe('Cart model', () => {
   beforeEach(() => db.sync({force: true}))
 
   describe('column definitions and validations', () => {
-    it('has a `name`, `price`, `description` and `inventory` ', async () => {
+    it('has a `guest`, `product_ids` ', async () => {
       const newCart = await Cart.create({
-        userid: 1,
         guest: false,
-        product_ids: [1,2,3]    
-    })
+        productIds: [1, 2, 3]
+      })
 
-      expect(newCart.userid).to.equal(1)
       expect(newCart.guest).to.deep.equal(false)
       expect(newCart.product_ids).to.equal()
+    })
+
+    it('is linked to a User', async () => {
+      const newCart = await Cart.create({
+        guest: false,
+        productIds: [1, 2, 3]
+      })
+      const newUser = await User.create({
+        email: 'cody@puppybook.com',
+        password: 'bones'
+      })
+
+      newUser.hasCart(newCart)
+
+      expect(newCart.userid).to.equal(newUser.id)
     })
   })
 })
