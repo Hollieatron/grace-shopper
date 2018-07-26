52973d9679f8667cfc3aa60a870bd18ee0813b64
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-07-25 22:42:00 -0500

    Add inventory flag

--- client/components/products/single-product-page.js
+++ client/components/products/single-product-page.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react'
 import {fetchProduct} from '../../store'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {Image, Button, Grid, Header, Segment, Label} from 'semantic-ui-react'
+import {
+  Image,
+  Button,
+  Grid,
+  Header,
+  Segment,
+  Label,
+  Icon
+} from 'semantic-ui-react'
 
 class SingleProductPage extends Component {
   componentDidMount() {
@@ -25,22 +33,31 @@ class SingleProductPage extends Component {
             </Grid.Column>
             <Grid.Column>
               <Header as="h2">{product.name}</Header>
-
-              <Label.Group tag>
-                <Label as="a">{'$' + product.price}</Label>
+              <Label.Group>
+                <Label as="a" size="large" tag>
+                  {'$' + product.price}
+                </Label>
+                {user.isAdmin ? (
+                  <Button
+                    as={Link}
+                    to={`/admin/products/edit/${product.id}`}
+                    style={styles.button}
+                    size="small"
+                  >
+                    <Icon name="edit" /> Edit
+                  </Button>
+                ) : (
+                  ''
+                )}
               </Label.Group>
 
               <Segment>{product.description}</Segment>
-              <Button basic color="red">
-                Add to Cart
+              <Button as="div" labelPosition="right">
+                <Button color="red">Add to Cart</Button>
+                <Label as="a" basic color="red" pointing="left">
+                  Only {product.inventory} left!
+                </Label>
               </Button>
-              {user.isAdmin ? (
-                <Button as={Link} to={`/admin/products/edit/${product.id}`} basic color="blue">
-                    Edit Product
-                </Button>
-              ) : (
-                ''
-              )}
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -63,6 +80,9 @@ const mapDispatch = dispatch => {
 const styles = {
   div: {
     marginTop: 40
+  },
+  button: {
+    marginLeft: 5
   }
 }
 
