2f4554c1c9139b5fae7c82cb951e4c7d30e1dffa
Eliot Davis <edavis221@gmail.com>
2018-07-31 13:54:31 -0500

    working on product card, changing to filter for order histories

--- client/components/products/product-card.js
+++ client/components/products/product-card.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 import {Card, Image, Rating} from 'semantic-ui-react'
+import {fetchReviewsOfProduct} from '../../store'
 
-const ProductCard = ({id, imageUrl, name, price, description}) => {
-  return (
-    <Card>
-      <Image src={imageUrl} />
-      <Card.Content>
-        <Card.Header>
-          <Link to={`/products/product/${id}`}>{name}</Link>
-        </Card.Header>
-        <Card.Meta>Price: ${price}</Card.Meta>
-        <Card.Description>{description.slice(0, 40)}.</Card.Description>
-        <Rating icon="star" defaultRating={4} maxRating={5} disabled />
-      </Card.Content>
-    </Card>
-  )
+const mapState = state => ({
+  reviews: state.reviews
+})
+
+const mapDispatch = dispatch => ({
+  getProductReviews: id => dispatch(fetchReviewsOfProduct(id))
+})
+
+class ProductCard extends React.Component {  
+  componentDidMount() {
+    const {id, getProductReviews} = this.props
+    getProductReviews(id)
+  }
+
+  render() {
+    const {id, imageUrl, name, price, description, reviews} = this.props
+    if(reviews && reviews.length) {
+
+    }
+    return (
+      <Card>
+        <Image src={imageUrl} />
+        <Card.Content>
+          <Card.Header>
+            <Link to={`/products/product/${id}`}>{name}</Link>
+          </Card.Header>
+          <Card.Meta>Price: ${price}</Card.Meta>
+          <Card.Description>{description.slice(0, 40)}.</Card.Description>
+          <Rating icon="star" defaultRating={ || 0} maxRating={5} disabled />
+        </Card.Content>
+      </Card>
+    )
+  }
 }
 
-export default ProductCard
+export default connect(mapState, mapDispatch)(ProductCard)
