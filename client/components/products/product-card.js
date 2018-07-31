c0ee14a80beb263f8f553f0863d18140ee6f452e
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-07-31 14:24:35 -0500

    add to cart from product card working

--- client/components/products/product-card.js
+++ client/components/products/product-card.js
@@ -1,21 +1,145 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {postCart, putCart, postGuestCart, putGuestCart} from '../../store'
 import {Link} from 'react-router-dom'
-import {Card, Image, Rating} from 'semantic-ui-react'
+import {Card, Image, Rating, Button, Icon} from 'semantic-ui-react'
+import {connect} from 'react-redux'
 
-const ProductCard = ({id, imageUrl, name, price, description}) => {
-  return (
-    <Card>
-      <Image src={imageUrl} />
-      <Card.Content>
-        <Card.Header>
-          <Link to={`/products/product/${id}`}>{name}</Link>
-        </Card.Header>
-        <Card.Meta>Price: ${price}</Card.Meta>
-        <Card.Description>{description.slice(0, 40)}.</Card.Description>
-        <Rating icon="star" defaultRating={4} maxRating={5} disabled />
-      </Card.Content>
-    </Card>
-  )
+const mapState = state => ({
+  user: state.user,
+  cart: state.cart
+})
+
+const mapDispatch = dispatch => {
+  return {
+    addToCart: input => dispatch(postCart(input)),
+    editProductQuantity: data => dispatch(putCart(data)),
+    addToGuestCart: product => dispatch(postGuestCart(product)),
+    editGuestQuantity: productId => dispatch(putGuestCart(productId))
+  }
 }
 
-export default ProductCard
+class ProductCard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      inCart: false,
+      inventoryReq: 0,
+      isGuest: true
+    }
+  }
+
+  componentDidMount() {
+    const {user, cart, product} = this.props
+    const {id} = product
+
+    // if user is logged in, set isGuest to false
+    if (user.id) {
+      this.setState({isGuest: false})
+    }
+
+    cart.forEach(elem => {
+      if (elem.productId === id) {
+        this.setState({inCart: true, inventoryReq: elem.inventoryReq})
+      }
+    })
+  }
+
+  addToCartSubmit(productId, userId) {
+    const {
+      addToCart,
+      editProductQuantity,
+      addToGuestCart,
+      editGuestQuantity,
+      product
+    } = this.props
+
+    let {inCart, inventoryReq, isGuest} = this.state
+    const quantity = inventoryReq + 1
+
+    // if the product isn't in the cart && it's a guest
+    if (!inCart && isGuest) {
+      addToGuestCart(product)
+      this.setState({inCart: true, inventoryReq: 1})
+    }
+
+    // if product is inCart and the user is a guest
+    if (inCart && isGuest) {
+      editGuestQuantity({productId, inventoryReq: quantity})
+      this.setState({inventoryReq: quantity})
+    }
+
+    // if the product isn't in the cart && user is logged in
+    if (!inCart && !isGuest) {
+      addToCart({productId, userId: userId})
+      this.setState({inCart: true, inventoryReq: 1})
+    }
+
+    // if the product is in the cart && user is logged in
+    if (inCart && !isGuest) {
+      editProductQuantity({quantity, productId, userId})
+      this.setState({inventoryReq: quantity})
+    }
+  }
+
+  renderAddProductButton() {
+    const {product, user} = this.props
+    const {inventory} = product
+    const {inventoryReq} = this.state
+
+    if (inventory === inventoryReq) {
+      return (
+        <Button
+          icon
+          compact
+          circular
+          negative
+          disabled
+          floated="right"
+          style={styles.button}
+        >
+          <Icon name="x" />
+        </Button>
+      )
+    } else {
+      return (
+        <Button
+          icon
+          compact
+          circular
+          floated="right"
+          style={styles.button}
+          onClick={() => this.addToCartSubmit(product.id, user.id)}
+        >
+          <Icon name="add to cart" />
+        </Button>
+      )
+    }
+  }
+
+  render() {
+    const {product} = this.props
+    const {id, imageUrl, name, price, description} = product
+    return (
+      <Card>
+        <Image src={imageUrl} />
+        <Card.Content>
+          <Card.Header>
+            <Link to={`/products/product/${id}`}>{name}</Link>
+            {product ? this.renderAddProductButton() : ''}
+          </Card.Header>
+          <Card.Meta>Price: ${price}</Card.Meta>
+          <Card.Description>{description.slice(0, 40)}.</Card.Description>
+          <Rating icon="star" defaultRating={4} maxRating={5} disabled />
+        </Card.Content>
+      </Card>
+    )
+  }
+}
+
+const styles = {
+  button: {
+    marginRight: 10
+  }
+}
+
+export default connect(mapState, mapDispatch)(ProductCard)
