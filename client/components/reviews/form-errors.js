07a35afc7b591010fb6055b1c71c601a54e4bc33
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-01 08:26:14 -0500

    add style to label

--- client/components/reviews/form-errors.js
+++ client/components/reviews/form-errors.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import {Label} from 'semantic-ui-react'
 
 const FormErrors = ({formErrors}) => (
   <div>
     {Object.keys(formErrors).map((fieldName, i) => {
       if (formErrors[fieldName].length > 0) {
         return (
-          <p key={i} style={{color: 'red'}}>
+          <Label pointing key={i} style={{color: 'red'}}>
             {fieldName.slice(0, 1).toUpperCase()}
             {fieldName.slice(1)} {formErrors[fieldName]}
-          </p>
+          </Label>
         )
       } else {
         return ''
